843e5e20cf50044c99562d365dccb45831ddf5ea
Thomas B <thomas77300@gmail.com>
2024-09-21 17:32:55 +0200

    Create CharredHorizons.java

---
+++ src/main/java/mrthomas20121/charred_horizons/CharredHorizons.java
@@ -0,0 +1,9 @@
+package mrthomas20121.charred_horizons;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CharredHorizons.mod_id)
+public class CharredHorizons {
+
+	public static final String mod_id = "charred_horizons";
+}
